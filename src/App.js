0d0f792285d32257c2025ad18393b3ae559a32a8
saibomma <saikiranreddy.b1995@gmail.com>
2024-01-07 17:37:37 -0600

    adding all the notebook files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import logo from './logo.svg';
+import FriendForm from './FriendForm';
+import FriendList from './FriendList';
+
+
+const App = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const storedFriends = JSON.parse(localStorage.getItem('friends'));
+    if (storedFriends) {
+      setFriends(storedFriends);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('friends', JSON.stringify(friends));
+  }, [friends]);
+
+  const handleAddFriend = (newFriend) => {
+    setFriends([...friends, newFriend]);
+  };
+
+  const handleEditFriend = (friendToUpdate) => {
+    const updatedFriends = friends.map((friend) =>
+      friend.id === friendToUpdate.id ? friendToUpdate : friend
+    );
+    setFriends(updatedFriends);
+  };
+
+  const handleDeleteFriend = (id) => {
+    const updatedFriends = friends.filter((friend) => friend.id !== id);
+    setFriends(updatedFriends);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Friend Notebook</h1>
+      <FriendForm onAddFriend={handleAddFriend} />
+      <FriendList
+        friends={friends}
+        onEditFriend={handleEditFriend}
+        onDeleteFriend={handleDeleteFriend}
+      />
     </div>
   );
-}
+};
 
 export default App;
